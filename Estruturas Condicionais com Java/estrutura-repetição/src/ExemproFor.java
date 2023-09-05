8e4fd2a0662bea4cd17c12a30cc4d5beb507b3c2
CosmodLima <cosmodlima016@gmail.com>
2023-09-04 21:49:37 -0300

    Estrutura

---
+++ Estruturas Condicionais com Java/estrutura-repetição/src/ExemproFor.java
@@ -0,0 +1,7 @@
+public class ExemproFor {
+    public static void main(String[] args) {
+        for(int carnerinho = 0; carnerinho <= 10; carnerinho += 2){
+        System.out.println("Contando carnerinhos: " + carnerinho);
+        }
+    }
+}
---
+++ Estruturas Condicionais com Java/estrutura-repetição/src/ExemproForArray.java
@@ -0,0 +1,16 @@
+public class ExemproForArray {
+    public static void main(String[] args) {
+        String alunos [] =  {"FELIPE","JONAS","JULIA","MARCOS"};
+
+        //Forma abreviada
+	    for(String aluno : alunos){
+            System.out.println("Nome do aluno é: " + aluno);
+        }
+
+    /*    
+	for (int x=0; x<alunos.length; x++) {
+		System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+	}
+    */
+    }
+}
